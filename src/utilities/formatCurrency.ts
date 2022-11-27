d56335072b8783cfa50147de7633f71037ee3fd8
terrobladeD <terrobladediao@gmail.com>
2022-11-27 23:37:56 +1100

    Adding dist

--- src/utilities/formatCurrency.ts
+++
@@ -1,9 +0,0 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
-    currency: "AUD",
-    style: "currency",
-  })
-  
-  export function formatCurrency(number: number) {
-    return CURRENCY_FORMATTER.format(number)
-  }
-  
